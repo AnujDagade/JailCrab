46fa8b8dead4caaa74347e82f6b0504149a3ad8d
Anuj Dagade <dagadeanuj@gmail.com>
2026-06-28 14:57:43 +0530

    refactor and changed new_root dir

--- src/isolate.rs
+++ src/isolate.rs
@@ -1,10 +1,54 @@
 use nix::sched::{CloneFlags, unshare};
 
+use nix::mount::{MntFlags, MsFlags, mount, umount2};
+use nix::unistd::{chdir, pivot_root};
+use std::env;
+use std::fs::{self};
+use std::path::Path;
 pub fn isolate_root() -> Result<(), String> {
     let flags = CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWUTS;
 
     unshare(flags).map_err(|e| format!("Failed to isolate process {}", e))?;
 
     println!("Successfully isolated into new namespaces!");
+    Ok(())
+}
+
+pub fn pivot_root_fs(root_path: &Path) -> Result<(), String> {
+    mount(
+        None::<&str>,
+        "/",
+        None::<&str>,
+        MsFlags::MS_PRIVATE | MsFlags::MS_REC,
+        None::<&str>,
+    )
+    .map_err(|e| format!("Failed to make root private {}", e))?;
+
+    //What will happen when if mounted path without isolation
+    mount(
+        Some(root_path),
+        root_path,
+        None::<&str>,
+        MsFlags::MS_BIND | MsFlags::MS_REC | MsFlags::MS_PRIVATE,
+        None::<&str>,
+    )
+    .map_err(|e| format!("Failed to mount new root path {}", e))?;
+
+    let old_root = root_path.join(".old_root");
+
+    if !old_root.exists() {
+        fs::create_dir_all(&old_root)
+            .map_err(|e| format!("Failed to create a dir to store old root {}", e))?;
+    }
+
+    let pid = std::process::id();
+    println!("Current PID: {}", pid);
+
+    pivot_root(root_path, &old_root).map_err(|e| format!("Failed to pivot root {}", e))?;
+
+    let curr_dir = env::current_dir().unwrap();
+
+    println!("Current directory: {}", curr_dir.display());
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,18 @@
 mod isolate;
-
-use isolate::isolate_root;
-use nix::libc::MS_REC;
-use nix::mount::{MntFlags, MsFlags, mount, umount2};
-use nix::unistd::{chdir, pivot_root};
 use std::env;
-use std::fmt::format;
-use std::fs::{self, create_dir_all};
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::Path;
 
 fn main() {
-    let _args: Vec<String> = env::args().collect();
-    isolate_root().unwrap();
-    pivot_root_fs(&PathBuf::from("/home/drcool/programming/new_root")).unwrap();
-}
+    let new_root = Path::new("/var/lib/jail_crab/roots");
 
-fn pivot_root_fs(root_path: &Path) -> Result<(), String> {
-    mount(
-        None::<&str>,
-        "/",
-        None::<&str>,
-        MsFlags::MS_PRIVATE | MsFlags::MS_REC,
-        None::<&str>,
-    )
-    .map_err(|e| format!("Failed to make root private {}", e))?;
-
-    //What will happen when if mounted path without isolation
-    mount(
-        Some(root_path),
-        root_path,
-        None::<&str>,
-        MsFlags::MS_BIND | MsFlags::MS_REC | MsFlags::MS_PRIVATE,
-        None::<&str>,
-    )
-    .map_err(|e| format!("Failed to mount new root path {}", e))?;
-
-    let old_root = root_path.join(".old_root");
-
-    if !old_root.exists() {
-        fs::create_dir_all(&old_root)
-            .map_err(|e| format!("Failed to create a dir to store old root {}", e))?;
+    if !new_root.exists() {
+        fs::create_dir_all(new_root)
+            .map_err(|e| format!("Failed to create new roots dir {}", e))
+            .unwrap();
     }
 
-    let pid = std::process::id();
-    println!("Current PID: {}", pid);
-
-    pivot_root(root_path, &old_root).map_err(|e| format!("Failed to pivot root {}", e))?;
-
-    let curr_dir = env::current_dir().unwrap();
-
-    println!("Current directory: {}", curr_dir.display());
-
-    Ok(())
+    let _args: Vec<String> = env::args().collect();
+    isolate::isolate_root().unwrap();
+    isolate::pivot_root_fs(new_root).unwrap();
 }
